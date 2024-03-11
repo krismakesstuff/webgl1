0149aacd47ff64dff8252ecc1fa0ed1e039deae4
Kris Crawford <11crawfordk@gmail.com>
2024-03-11 10:30:56 -0400

    messing around with examples

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+
+
+function updateSpeed(newSpeed){
+    console.log(newSpeed);
+
+    ySpeed = newSpeed;
+    xSpeed = newSpeed;
+
+    console.log("xSpeed= " + xSpeed);
+    console.log("ySpeed= " + ySpeed);
+}
